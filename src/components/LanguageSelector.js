9939ca546e04cd0ad12cec74f5ad9d8316549f44
daniyalibrahim <daniyal.ibrahim10@gmail.com>
2022-06-22 15:39:29 +0200

    style lang selector

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -1,26 +1,44 @@
 import React, { useContext } from 'react';
+import {css} from "@emotion/react";
 
 import { languageOptions } from '../languages';
 import { LanguageContext } from '../containers/Language';
+import { useThemeContext } from '../contexts/ThemeContext';
 
-function getCountryCode(countryName) {
-  return 'https://lipis.github.io/flag-icon-css/flags/4x3/'+countrycodedict[countryName]+'.svg';
+const LangSelectStyle =(colors,isLight)=>css`
+
+  .select {
+    background-color:transparent;
+    color: ${isLight ? 'inherit' : colors.gray[8]};
+    border-radius:0%;
+    height:5vh;
+    width:70px;
+    margin-right:10px;
+  }
+  select option {
+    background-color: transparent;
+    color: black;
 }
+`;
 
 export default function LanguageSelector() {
+  const {colors,isLight} = useThemeContext();
   const { userLanguage, userLanguageChange } = useContext(LanguageContext);
 
   // set selected language by calling context method
   const handleLanguageChange = e => userLanguageChange(e.target.value);
 
   return (
+    <div css={[LangSelectStyle(colors,isLight)]}>
     <select
       onChange={handleLanguageChange}
       value={userLanguage}
+      className="select"
     >
       {Object.entries(languageOptions).map(([id, name]) => (
-        <option key={id} value={id}>{name}</option>
+        <option  key={id} value={id}>{name}</option>
       ))}
     </select>
+    </div>
   );
 };
